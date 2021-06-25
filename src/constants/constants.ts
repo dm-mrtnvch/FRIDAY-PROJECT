6e0300bc65b6798255fd5f6cc7fbd3964ba7e216
DM-MRTNVCH <dmvshn@gmail.com>
2021-06-25 17:48:49 +0300

    Revert "routes"
    This reverts commit 05859f784054938dac8dcef93bd53291423a879e.

--- src/constants/constants.ts
+++
@@ -1,6 +0,0 @@
-export const REGISTER_PATH = '/register'
-export const LOGIN_PATH = '/login'
-export const PROFILE_PATH = '/profile'
-export const RECOVER_PASSWORD_PATH = '/recover-password'
-export const SET_NEW_PASSWORD_PATH = '/set-new-password'
-
