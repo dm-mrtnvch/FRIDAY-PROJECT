d49f5ae46a0d1a1d4e569adfd6f212651ef6a00e
gerasimenko.valery@gmail.com <gerasimenko.valery@gmail.com>
2021-07-02 17:34:25 +0300

    feature/registration page

--- src/components/user/credentials/registration/registration.tsx
+++ src/components/user/credentials/registration/registration.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, {ChangeEvent, useCallback, useState} from 'react'
+import {Redirect} from 'react-router-dom';
+import {registrationAPI} from '../../../../api/RegistrationAPI';
+import {useDispatch, useSelector} from 'react-redux';
+import {registerTC} from '../../../../redux/reducers/registration-reducers';
+import {LOGIN_PATH} from '../../../../constants/constants';
 
-const Registration = () => {
+const Registration = React.memo(() => {
+
+    const dispatch = useDispatch()
+    const isLoading = useSelector<any>(state => state.registration.isLoading)
+    const response = useSelector<any>(state => state.registration.response)
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    // const [disable, setDisable] = useState<boolean>(false)
+
+    const onEmailChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }, [setEmail])
+    const onPasswordChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value)
+    }, [setPassword])
+
+    const onSubmitForm = useCallback(() => {
+        dispatch(registerTC(email, password))
+    }, [email, password, dispatch])
+
+    // Object is of type 'unknown'.  TS2571 - в response.status === 201 ругается на response.
+    // if (response && response.status === 201) {
+    if (response) {
+        return <Redirect to={LOGIN_PATH}/>
+    } else {
+
+    }
+
     return (
-        <div>REGISTRATION</div>
+        <>
+            <div>REGISTRATION</div>
+            {
+                isLoading
+                    ? <div>L-O-A-D-I-N-G...</div>
+                    : ''
+            }
+            <input type="email" placeholder='email' onChange={onEmailChange} value={email}/>
+            <br/>
+            <input type="password" placeholder='password' onChange={onPasswordChange} value={password}/>
+            <br/>
+            {/*isLoading ? true : false тож самое что и  !!isLoading*/}
+            <button onClick={onSubmitForm} disabled={!!isLoading}>send</button>
+        </>
     )
-}
+})
 
 export default Registration
+
+
+// /^[\\w]{1}[\\w-\\.]*@[\\w-]+\\.[a-z]{2,7}$/i.test('x@x.xx')
--- src/redux/reducers/registration-reducers.ts
+++ src/redux/reducers/registration-reducers.ts
@@ -1,14 +1,60 @@
+import {Dispatch} from 'redux';
+import {registrationAPI} from '../../api/RegistrationAPI';
+
+const REGISTER_START = 'REGISTER_START'
+const REGISTER_END = 'REGISTER_END'
 
 
-const initialState = {
+type InitialStateType = {
+    isLoading: boolean,
+    response: null | any
+}
 
+const initialState: InitialStateType = {
+    isLoading: false,
+    response: null
 }
 
 const registrationReducer = (state = initialState, action: any) => {
-    switch (action.type){
+    switch (action.type) {
+        case REGISTER_START: {
+            return {
+                ...state,
+                isLoading: true,
+                response: null
+            }
+        }
+        case REGISTER_END: {
+            return {
+                ...state,
+                isLoading: false,
+                response: action.response
+            }
+        }
         default:
             return state
     }
+}
+
+
+const registerStartAC = () => ({
+    type: REGISTER_START
+})
+
+
+const registerEndAC = (response: any) => ({
+    type: REGISTER_END,
+    response
+})
+
+
+export const registerTC = (email: string, password: string) => (dispatch: Dispatch) => {
+    dispatch(registerStartAC())
+    registrationAPI.register(email, password)
+        .then(response => {
+            dispatch(registerEndAC(response))
+        })
+
 }
 
 export default registrationReducer
